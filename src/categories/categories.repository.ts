4637721cdd26025e7e2d03f0db41cfb6f5804860
Lucas Gabriel <lucas1020111940@hotmail.com>
2025-04-02 20:57:43 -0300

    refactor: add without archives

--- src/categories/categories.repository.ts
+++ src/categories/categories.repository.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoriesRepository {
-  private categories: [] = [];
+  private categories = [
+    {
+      id: 'uuid.xpto',
+      name: 'test',
+      desc: 'desc cat',
+    },
+  ];
 
-  getAll() {
+  async getAll() {
     return this.categories;
   }
 }
