57865761cc7537749eea248b35aaf0d3dfd24cb1
Lucas Gabriel <lucas1020111940@hotmail.com>
2025-04-02 19:36:03 -0300

    feat:categories module and controller

---
+++ src/categories/categories.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CategoriesController } from './categories.controller';
+
+@Module({
+  imports: [],
+  controllers: [CategoriesController],
+  providers: [],
+})
+export class CategoriesModule {}
